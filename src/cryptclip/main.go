be51d0c8e3d88658deee50afbee6e7d13a12256f
ekeel <eric.keeling@gmail.com>
2019-12-02 01:45:30 -0800

    Updating arguments

--- src/cryptclip/main.go
+++ src/cryptclip/main.go
@@ -12,13 +12,15 @@ import (
 	"github.com/atotto/clipboard"
 )
 
-func main() {
-	timeoutPtr := flag.Duration("t", 0, "Erase clipboard after timeout.  Durations are specified like \"20s\" or \"2h45m\".  0 (default) means never erase.")
-	encryptPtr := flag.Bool("enc", false, "encrypt the provided string")
-	decryptPtr := flag.Bool("dec", false, "decrypt the provided string")
-	keyPtr := flag.String("key", "", "The key to use for encryption")
-	msgPtr := flag.String("msk", "", "The message to encrypt/decrypt")
+var (
+	timeoutPtr = flag.Duration("t", 0, "Erase clipboard after timeout.  Durations are specified like \"20s\" or \"2h45m\".  0 (default) means never erase.")
+	encryptPtr = flag.Bool("e", false, "encrypt the provided string")
+	decryptPtr = flag.Bool("d", false, "decrypt the provided string")
+	keyPtr     = flag.String("key", "", "The key to use for encryption")
+	msgPtr     = flag.String("msg", "", "The message to encrypt/decrypt")
+)
 
+func main() {
 	flag.Parse()
 
 	if *encryptPtr && *decryptPtr {
